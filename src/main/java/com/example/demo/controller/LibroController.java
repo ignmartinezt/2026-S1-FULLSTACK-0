460434f2b2d62cff56a82ac72244ad4db89fa343
Ignacio <ign.martinezt@duocuc.cl>
2026-03-23 12:04:02 -0300

    commit 2

--- src/main/java/com/example/demo/controller/LibroController.java
+++ src/main/java/com/example/demo/controller/LibroController.java
@@ -1,5 +1,55 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Libro;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/libros")
+
+
 public class LibroController {
+
+
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List listarLibros() {
+        return libroService.getLibros();
+    }
+
+    @PostMapping
+    public String agregarLibro(@RequestBody Libro libro) {
+
+        return libroService.saveLibro(libro);
+    }
+    
+    @PutMapping("{id}")
+    public String actualizarLibro(@PathVariable String id, @RequestBody String libro) {
+        //TODO: process PUT request
+        
+        return libroService.updateLibro(libro);
+    }
+
+    @DeleteMapping("{id}")
+    public String eliminarLibro(@PathVariable int id){
+        return libroService.deleteLibro(id);
+    }
 
 }
--- src/main/java/com/example/demo/model/Libro.java
+++ src/main/java/com/example/demo/model/Libro.java
@@ -1,5 +1,16 @@
 package com.example.demo.model;
 
-public class Libro {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data
+
+public class Libro {
+    private int id;
+    private int isbn;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private int lanzamiento;
 }
